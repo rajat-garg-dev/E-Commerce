d4e6ca1e993931769688eb04df3c10181c2702ce
rajat-garg-dev <gargrajat781@gmail.com>
2024-02-26 13:28:05 +0530

    created navbar and home page half section

---
+++ frontend/src/customer/components/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../homecarasoule/Maincarasoule'
+
+export default function HomePage() {
+  return (
+    <div>
+      <div>
+<MainCarousel />
+      </div>
+      <div>
+        other section
+      </div>
+    </div>
+  )
+}
