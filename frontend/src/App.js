46cdfac040f0ce096133f41499c99d41bed8a222
rajat-garg-dev <gargrajat781@gmail.com>
2024-03-12 18:10:14 +0530

    filter is updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./customer/components/navigation/navigation";
 import HomePage from "./customer/components/pages/home/HomePage";
+import Product from "./customer/components/product/Product";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
       </div>
     </div>
   );
